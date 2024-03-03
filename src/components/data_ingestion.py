c8262c2845a7d85e36685ba4a8081c0e5d07d24c
pesricha <naman_p@me.iitr.ac.in>
2024-03-03 19:29:49 +0530

    Added data_transformation.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -46,4 +47,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path= obj.initiate_data_ingestion()
+    
+    data_transformation =  DataTransformation()
+    data_transformation.initiate_data_transformation(train_path, test_path)
